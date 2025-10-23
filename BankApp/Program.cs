463c251c8aa57560fe8310c6ec1813281f4959bc
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2025-10-23 09:33:23 +0200

    deleted code from main

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -8,13 +8,6 @@ namespace BankApp
         {
 			// Log-in metod
 			// INGEN LOGIK HÄR ENDAST STARTA IGÅNG APPEN :)
-			BankAccountBase bankaccount1 = new BankAccountBase(1000);
-			Console.WriteLine(bankaccount1.AccountNumber);
-
-			BankAccountBase bankaccount2 = new BankAccountBase(2000);
-			Console.WriteLine(bankaccount2.AccountNumber + bankaccount2.Balance);
-			BankAccountBase bankaccount3 = new BankAccountBase(30.00);
-			Console.WriteLine(bankaccount3.AccountNumber + bankaccount3.Balance);
 
 		}
     }
